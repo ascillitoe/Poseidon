b5ec6da81e278223434421ad506434788dadcf95
Ashley Scillitoe <as2341@cam.ac.uk>
2017-09-17 18:36:51 +0100

    Starting off main.F and constants.inc

---
+++ libs/OPS/scripts/test_all.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+source ./source_intel #default source to set environment vars
+#exit script if any error is encountered during the build or
+#application executions.
+set -e
+echo $OPS_INSTALL_PATH
+cd $OPS_INSTALL_PATH
+
+echo "************Testing C Applications *****************"
+cd ../apps/c/CloverLeaf/
+cd ../CloverLeaf/
+./test.sh
+cd ../CloverLeaf_3D/
+./test.sh
+cd ../CloverLeaf_3D_HDF5/
+./test.sh
+cd ../TeaLeaf/
+./test.sh
+cd ../poisson/
+./test.sh
+cd ../multiDim/
+./test.sh
+cd ../multiDim3D/
+./test.sh
+cd ../shsgc/
+./test.sh
+cd ../mb_shsgc/Max_datatransfer
+./test.sh
+cd ../../multiDim_HDF5
+./test.sh
+
+echo "************Testing Fortran Applications *****************"
+cd $OPS_INSTALL_PATH
+cd ../apps/fortran/shsgc
+./test.sh
+cd ../poisson
+./test.sh
+cd ../multiDim
+./test.sh
+cd ../multiDim3D
+./test.sh
---
+++ src/main.F90
@@ -0,0 +1,30 @@
+! -----------------------------------------------------------------------------
+! CFD4turbo  - Compact Finite Differences for turbomachinery
+!
+! Author(s) - Ashley Scillitoe
+!
+! This is a multi-block structured CFD code using 6th order finite
+! differences and low storage RK to solve the compressible Navier-Stokes 
+! equations. 
+!
+! History:
+! 16/09/17  - First build.                                             - as2341
+!
+! -----------------------------------------------------------------------------
+
+program main
+
+  use OPS_Fortran_Reference
+  use OPS_FORTRAN_HDF5_DECLARATIONS
+  use OPS_CONSTANTS
+  use constants
+
+  use, intrinsic :: ISO_C_BINDING
+
+  implicit none
+
+
+
+  
+
+end program
